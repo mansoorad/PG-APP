0fb5c0f58f274bb56e71e238de1aa192a3a725e3
mansoorjavadeveloper <mansoorjavadeveloper@gmail.com>
2022-12-07 12:19:47 +0530

    added starter files

---
+++ src/main/java/com/ty/pg_app/pgboot_app/PgbootAppApplication.java
@@ -0,0 +1,13 @@
+package com.ty.pg_app.pgboot_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgbootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PgbootAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ty/pg_app/pgboot_app/PgbootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ty.pg_app.pgboot_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PgbootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
